f6b673c5dcb17f7236030c8e34da5e31eac4e043
MakoStrwlkr <72935357+MakoStrwlkr@users.noreply.github.com>
2021-02-21 18:37:40 +0530

    Update training-chatty.py

--- training-chatty.py
+++ training-chatty.py
@@ -1,16 +1,25 @@
-"""UofTHacks"""
+"""
+UofTHacks 2021 Project Chatty
+Module for Training code
+"""
 
 import tensorflow as tf
-import clean_data
+
+from clean_data import answers
+from clean_data import questions
 
 import numpy as np
 from keras.optimizers import SGD
 import random
 import nltk
 from nltk.stem import WordNetLemmatizer
+import pickle
 
 NUM_EPOCHS = 80
 BATCH_SIZE = 5
+
+# nltk.download('punkt')
+# nltk.download('wordnet')
 
 lemmatizer = WordNetLemmatizer()
 
@@ -18,8 +27,6 @@ words = []
 categories = []
 docs = []
 
-questions = clean_data.clean_questions
-answers = clean_data.clean_answers
 unnecessary = ['?', '!', '.']
 
 for i in range(len(questions)):
@@ -38,11 +45,20 @@ for i in range(len(questions)):
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in unnecessary]
 words = sorted(list(set(words)))
 
-classes = sorted(list(set(categories)))
+categories = sorted(list(set(categories)))
+
+# print(len(docs), "docs")
+#
+# print(len(categories), "categories", categories)
+#
+# print(len(words), "unique words", words)
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(categories, open('categories.pkl', 'wb'))
 
 # Initialize empty list meant to store training data
 training = []
-output_empty = [] * len(classes)
+output_empty = [] * len(categories)
 
 # From here on, it is the standard method to train a model
 for doc in docs:
@@ -58,11 +74,11 @@ for doc in docs:
 
     # output is a '0' for each tag and '1' for current tag (for each pattern)
     output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
+    output_row[categories.index(doc[1])] = 1
 
     training.append([bag, output_row])
 
-# randomly rearrange training data
+# Randomly rearrange training data
 random.shuffle(training)
 
 # Convert to np.array, which helps in specifying shape of hidden layers
@@ -76,25 +92,25 @@ print("Training data created")
 # Create model with 3 layers.
 # First and second layers have 128 neurons.
 # 3rd layer is output, and contains as many neurons as there are answers
-# Reply predicted using softmax
+# Prediction uses softmax as the activator function
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten())
-# model.add(Dense(128, input_shape=(len(x_train[0]),), activation='relu'))
+model.add(tf.keras.layers.Dense(128, input_shape=(len(x_train[0]),), activation='relu'))
+# model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(128, activation='relu'))
-
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-
+model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(len(y_train[0]), activation='softmax'))
 
 # Compile model.
 # For this chatbot, we choose stochastic gradient descent
-stochastic = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+stochastic = SGD(lr=0.01, decay=1e-4, momentum=0.9, nesterov=True)
 model.compile(optimizer=stochastic, loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Model fitting
 hist = model.fit(np.array(x_train), np.array(y_train), epochs=NUM_EPOCHS,
                  batch_size=BATCH_SIZE, verbose=1)
 # Note: Make verbose=0 after verifying program works
-model.save('chatbot_model.h5', hist)
+model.save('chatty.h5', hist)
 
-print("model created")
+print("Model complete")
