233480bd80d9c5f9c567b9c3716d9ef3c41e0836
mmojan <66796820+mmojan@users.noreply.github.com>
2021-02-21 02:04:50 -0500

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -1,24 +1,24 @@
 def conversation_list() -> list:
-    """ read movie_conversations and make an id list
+    """ read movie_conversations and make a list of ids of lines
     """
-    lines = open('movie_conversations.txt').read().split('\n')
-    id_list = []
+    lines = open('movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n')
+    conv_id = []
     for line in lines[:-1]:
         line = line.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
-        id_list.append(line.split(','))
-    return id_list
+        conv_id.append(line.split(','))
+    return conv_id
  
-
+    
 def line_dict() -> dict:
-    """ read movie_lines and return id: line dict.
+    """ read movie_lines and return id: line dict
     """
-    lines = open('movie_lines.txt').read().split('\n')
+    lines = open('movie_lines.txt', encoding='utf-8', errors='ignore').read().split('\n')
     id_line = {}
     for line in lines:
         line = line.split(' +++$+++ ')
-        id_line[line[0]] = line[4]
+        if len(line) == 5:
+            id_line[line[0]] = line[4]
     return id_line
-    # TODO: fix UnicodeDecodeError
     
     
 def conversation_file(conv_id, id_line):
@@ -33,3 +33,16 @@ def conversation_file(conv_id, id_line):
             f.write('\n')
         f.close()
         id += 1
+ 
+
+def q_and_a(conv_id, id_line) -> tuple:
+    """ input: the output of the first two functions
+    return lists of q and as
+    """
+    questions = []
+    answers = []
+    for conv in conv_id:
+        for i in range(len(conv) - 1):
+            questions.append(id_line[conv[i]])
+            answers.append(id_line[conv[i + 1]])
+    return questions, answers
